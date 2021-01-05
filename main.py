5f5cdf7789c1834ba21e48c21a076e2fbafb3af9
jod35 <jodestrevon@gmail.com>
2021-01-05 15:05:15 +0300

    finished

---
+++ main.py
@@ -0,0 +1,129 @@
+from flask import (
+		Flask,
+		jsonify,
+		make_response,
+		request,
+		render_template
+		)
+
+from marshmallow import fields, Schema
+from flask_fontawesome import FontAwesome
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+import os
+
+BASEDIR=os.path.dirname(os.path.realpath(__file__))
+
+app=Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"]='sqlite:///'+os.path.join(BASEDIR,'tasks.db')
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False 
+#app.config["SQLALCHEMY_ECHO"]=True
+
+
+db=SQLAlchemy(app)
+fa=FontAwesome(app)
+
+#task model
+class Task(db.Model):
+	id=db.Column(db.Integer,primary_key=True)
+	name=db.Column(db.String,nullable=False)
+	description=db.Column(db.Text,nullable=False)
+	date=db.Column(db.DateTime,default=datetime.utcnow)
+	
+	def __repr__(self):
+		return f"task => {self.name}"
+		
+	def save(self):
+		db.session.add(self)
+		db.session.commit()
+		
+		
+	def delete(self):
+		db.session.delete(self)
+		db.session.commit()
+#the model's schema
+class TaskSchema(Schema):
+	id=fields.Int()
+	name=fields.Str()
+	description=fields.Str()
+	date=fields.DateTime()
+	
+#welcome message
+@app.route('/')
+def index():
+	tasks=Task.query.order_by(Task.id.desc()).all()
+	return render_template('index.html',tasks=tasks)
+
+
+#get all tasks	
+@app.route('/api/tasks',methods=['GET'])
+def get_all_tasks():
+	tasks=Task.query.all()
+	response=TaskSchema(many=True).dump(tasks)
+	
+	return make_response(jsonify({"tasks":response}),200)
+
+#get a task by id
+@app.route('/api/task/<int:id>',methods=['GET'])
+def get_a_task(id):
+	task=Task.query.get_or_404(id)
+	
+	response=TaskSchema().dump(task)
+	
+	return make_response(jsonify({"task":response}),200)
+	
+#create a new task
+@app.route('/api/tasks',methods=['POST'])
+def create_task():
+	data=request.get_json()
+	
+	new_task=Task(name=data.get('name'),description=data.get('description'))
+	
+	new_task.save()
+	response=TaskSchema().dump(new_task)
+	return make_response(jsonify({"task":response,"success":True,"message":"CREATED"}),200)
+	
+#update a task
+@app.route('/api/task/<int:id>',methods=['PUT'])
+def update_task(id):
+	task=Task.query.get_or_404(id)
+	data=request.get_json()
+	task.name=data.get('name')
+	task.description=data.get('description')
+	db.session.commit()
+	
+	response=TaskSchema().dump(task)
+	return make_response(jsonify({"task":response,"success":True,"message":"UPDATED"}),200)
+	
+	
+#delete a task
+@app.route('/api/task/<int:id>',methods=['DELETE'])
+def delete_a_task(id):
+	task=Task.query.get_or_404(id)
+	db.session.delete(task)
+	db.session.commit()
+	response=TaskSchema().dump(task)
+	return make_response(jsonify({"task":response,"success":True,"message":"DELETED"}),200)
+	
+	
+
+	
+#handle a 404 error
+@app.errorhandler(404)
+def not_found(err):
+	return make_response(jsonify({"message":"Resource Not Found"}),200)
+	
+#handle a 500 error
+@app.errorhandler(500)
+def internal_error(err):
+	return make_response(jsonify({"message":"Internal Server Error"}),500)
+	
+	
+	
+if __name__ == '__main__':
+	#db.create_all()
+	app.run()
+		
+		
+	
---
+++ static/main.js
@@ -0,0 +1,156 @@
+const taskForm = document.querySelector("form");
+const appHeader = document.querySelector("header");
+const taskContainer = document.querySelector(".tasks");
+const tasks = document.querySelectorAll(".task");
+const noTaskContainer = document.querySelector(".notasks");
+const countContainer = document.querySelector(".count");
+const deleteButtons = document.querySelectorAll(".delete-btn");
+const taskIds = document.querySelectorAll(".id");
+const updateModal = document.querySelector("#update");
+const updateButtons = document.querySelectorAll(".update-btn");
+const updateForm = document.querySelector("#form-update");
+const notificationModal = document.querySelector(".noti-modal");
+const message = document.querySelector(".message");
+
+let p = document.createElement("p");
+
+p.classList.add("count-all");
+p.innerText = `Tasks: ${tasks.length}`;
+
+countContainer.appendChild(p);
+
+window.onload = function () {
+  scrollWindow();
+};
+
+function scrollWindow() {
+  if (document.documentElement.scrollTop > 100) {
+    appHeader.style.backgroundColor = "dodgerblue";
+  }
+}
+
+function closeModal(id) {
+  let el = document.getElementById(id);
+
+  el.style.display = "none";
+}
+
+if (tasks.length == 0) {
+  noTaskContainer.style.display = "block";
+} else if (tasks.length > 0) {
+  noTaskContainer.style.display = "none";
+}
+
+//ading a task
+taskForm.addEventListener("submit", (e) => {
+  noTaskContainer.style.display = "none";
+  let taskData = new FormData(taskForm);
+
+  let new_task = {
+    name: taskData.get("name"),
+    description: taskData.get("description"),
+  };
+
+  let API_URL = `/api/tasks`;
+
+  fetch(API_URL, {
+    body: JSON.stringify(new_task),
+    method: "POST",
+    headers: { "content-type": "application/json" },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      let html = `
+                <h3 class="task-name">
+                    ${data.task.name}
+                </h3>
+                <p class="task-desc">
+                   ${data.task.description}
+                </p>
+                <a href="#" class="delete-btn"><i class="fa fa-trash" aria-hidden="true"></i></a>
+                <a href="#" class="update-btn"><i class="fa fa-pen" aria-hidden="true"></i></a>
+            `;
+      let newTask = document.createElement("div");
+
+      newTask.classList.add("task");
+
+      newTask.innerHTML = html;
+
+      taskContainer.insertBefore(newTask, taskContainer.childNodes[0]);
+      notificationModal.style.display = "block";
+      message.innerText = data.message;
+
+      setTimeout(() => {
+        location.reload();
+      }, 3000);
+    });
+
+  taskForm.reset();
+  e.preventDefault();
+});
+
+//deleting a task
+for (let i = 0; i < tasks.length; i++) {
+  deleteButtons[i].addEventListener("click", () => {
+    let RESOURCE_URL = `/api/task/${taskIds[i].innerText}`;
+
+    fetch(RESOURCE_URL, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        notificationModal.style.display = "block";
+        message.innerText = data.message;
+        tasks[i].style.display = "none";
+        setTimeout(() => {
+          location.reload();
+        }, 2000);
+      });
+  });
+}
+
+//display update form with data
+for (let i = 0; i < tasks.length; i++) {
+  updateButtons[i].addEventListener("click", () => {
+    let RESOURCE_URL = `/api/task/${taskIds[i].innerText}`;
+    updateModal.style.display = "block";
+
+    fetch(RESOURCE_URL, { method: "GET" })
+      .then((res) => res.json())
+      .then((data) => {
+        document.querySelector("#update-name").value = data.task.name;
+        document.querySelector("#update-desc").value = data.task.description;
+      });
+  });
+}
+
+for (let i = 0; i < tasks.length; i++) {
+  updateForm.addEventListener("submit", (e) => {
+    let RESOURCE_URL = `/api/task/${taskIds[i].innerText}`;
+
+    let updatedData = new FormData(updateForm);
+
+    let updatedTask = {
+      name: updatedData.get("name"),
+      description: updatedData.get("description"),
+    };
+    updateModal.style.display = "none";
+    fetch(RESOURCE_URL, {
+      method: "PUT",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(updatedTask),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        notificationModal.style.display = "block";
+        message.innerText = data.message;
+
+        setTimeout(() => {
+          location.reload();
+        }, 3000);
+      });
+
+    e.preventDefault();
+  });
+}
